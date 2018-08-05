843b69d3d5000a3cd061ffe4c102fa754c3d5ae8
huxiaodongofcode <895756412@qq.com>
2018-08-05 15:23:56 +0800

    加入k选取计算流程

--- sort/k_selection.cpp
+++ sort/k_selection.cpp
@@ -1,6 +1,20 @@
 /*
 *K-Selection
 *2018年8月4日 23:26:28
+*算法主要计算流程
+select(A,k)
+输入：规模为n的无序序列A，秩K>=0
+输出：A所对应有序序列中秩为k的元素
+{
+	if(n==|A|<Q) return trivialSelect(A,k);//递归基：序列规模不大时直接使用蛮力算法
+	将A均匀地划分为n/Q个子序列，各含Q个元素;//Q为5
+	各子序列分别排序，计算中位数，并将这些中位数组成一个序列;//可采用任何排序算法
+	通过递归调用select()，计算出中位数序列的中位数，记做M;
+	根据其相对于M的大小，将A中元素分为三个子集：L(小于),E(相等),G(大于);
+	if(|L|>=k) return select(L,k);
+	else if(|L|+|E|>=k) return M;
+	else return select(G,k-|L|-|E|);
+}//最坏情况下依然为O(n)
 */
 #include<iostream>
 #include<vector>
